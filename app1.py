af4f62aecdda4b9e8f59ef96a0d23bfce179fd3a
unknown <ak06598909@gmail.com>
2024-12-07 16:08:52 +0500

    FirstLecture

--- app1.py
+++ app1.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
+'''
+Creating a app object of flas application
+'''
 app=Flask(__name__)
 
+
+''' Decorator it will take two arguments , rule and options'''
 @app.route('/')
 def welcome():
     return "Welocme to my flask app"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    '''Running the object app , set debug = True'''
+    app.run(debug=True)  
