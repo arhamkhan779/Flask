7e98c705337c598353c41cc26a564ccfdbfaf0a7
unknown <ak06598909@gmail.com>
2024-12-09 14:47:20 +0500

    Third Lecture

---
+++ app3.py
@@ -0,0 +1,33 @@
+'''
+How to Build URL dynamically
+Flask variables and URL Builder
+app.route(  URL , Methods) --->Decorator
+'''
+
+from flask import Flask,redirect,url_for
+
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome to my app"
+
+@app.route('/success/<int:score>')
+def success(score):
+    return f"The Person has Passed and the score is {score}"
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return f"The Person has Failed and the score is {score}"
+
+@app.route("/results/<int:marks>")
+def results(marks):
+    result=""
+    if marks >= 50:
+       result = "sucess"
+    else:
+        result = "fail"
+    return redirect(url_for(result,score=marks))
+
+if __name__ == "__main__":
+    app.run(debug=True)
